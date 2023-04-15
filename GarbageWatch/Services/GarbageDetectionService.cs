c9f2c579aafea92122feb96d17ff997e4a200e98
adityaoberai <adityaoberai1@gmail.com>
2023-04-15 23:58:01 +0530

    Add garbage detection service

---
+++ GarbageWatch/Services/GarbageDetectionService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
+using GarbageWatch.Helpers;
+using System.Net;
+
+namespace GarbageWatch.Services
+{
+    public class GarbageDetectionService
+    {
+        private string imagePath;
+        private List<string> tags;
+        public GarbageDetectionService(string ImagePath)
+        {
+            imagePath = ImagePath;
+            tags = new List<string>();
+        }
+
+        public async Task<List<string>> DetectGarbage()
+        {
+            ComputerVisionClient client =
+              new ComputerVisionClient(new ApiKeyServiceClientCredentials(CognitiveServicesHelper.AzureComputerVisionAPIKey))
+              { Endpoint = CognitiveServicesHelper.AzureComputerVisionAPIEndpoint };
+
+            List<VisualFeatureTypes?> features = new List<VisualFeatureTypes?>()
+            {
+                VisualFeatureTypes.Tags
+            };
+
+            using (Stream analyzeImageStream = File.OpenRead(imagePath))
+            {
+
+                ImageAnalysis results = await client.AnalyzeImageInStreamAsync(analyzeImageStream, visualFeatures: features);
+
+                foreach (var tag in results.Tags)
+                {
+                    tags.Add(tag.Name);
+                }
+            }
+            return tags;
+        }
+    }
+}
